144cb4c81a5fb2e468305ec321dcab683f252a3a
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-01-18 12:34:12 +0100

    Fix tab control to not share instances of query sheet view

---
+++ CosmosDBStudio/View/Controls/QuerySheetTabControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CosmosDBStudio.View.Controls
+{
+    public class QuerySheetTabControl : TabControl
+    {
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is QuerySheetView;
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new QuerySheetView();
+        }
+    }
+}
--- CosmosDBStudio/View/QuerySheetView.xaml.cs
+++ CosmosDBStudio/View/QuerySheetView.xaml.cs
@@ -1,24 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace CosmosDBStudio.View
 {
-    /// <summary>
-    /// Interaction logic for QuerySheetView.xaml
-    /// </summary>
-    public partial class QuerySheetView : UserControl
+    public partial class QuerySheetView : TabItem
     {
         public QuerySheetView()
         {
