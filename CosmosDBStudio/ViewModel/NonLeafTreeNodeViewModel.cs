59227ffab6ef8497fa927195cc2f2409cfe0a8bf
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-12-05 01:02:21 +0100

    Handle errors while loading children nodes

--- CosmosDBStudio/ViewModel/NonLeafTreeNodeViewModel.cs
+++ CosmosDBStudio/ViewModel/NonLeafTreeNodeViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using EssentialMVVM;
 
 namespace CosmosDBStudio.ViewModel
@@ -35,20 +37,68 @@ namespace CosmosDBStudio.ViewModel
             }
         }
 
+        private string _error;
+        public string Error
+        {
+            get => _error;
+            set => Set(ref _error, value).AndNotifyPropertyChanged(nameof(HasError));
+        }
+
+        public bool HasError => !string.IsNullOrEmpty(Error);
+
         private Task _loadChildrenTask;
 
         private async Task InternalLoadChildrenAsync()
         {
-            _children = await LoadChildrenAsync();
+            try
+            {
+                Error = null;
+                _children = await LoadChildrenAsync();
+            }
+            catch (Exception ex)
+            {
+                await Task.Yield();
+                Error = "Error " + ex.Message;
+                _children = new[] { new ErrorTreeNodeViewModel(this, ex) };
+            }
+
             _loadChildrenTask = null;
             OnPropertyChanged(nameof(Children));
         }
 
         protected abstract Task<IEnumerable<TreeNodeViewModel>> LoadChildrenAsync();
+
+        public void RetryLoadChildren()
+        {
+            _children = null;
+            OnPropertyChanged(nameof(Children));
+        }
     }
 
     sealed class PlaceholderTreeNodeViewModel : TreeNodeViewModel
     {
         public override string Text => "Loading...";
+    }
+
+    sealed class ErrorTreeNodeViewModel : TreeNodeViewModel
+    {
+        private readonly NonLeafTreeNodeViewModel _parent;
+        private readonly Exception _exception;
+
+        public ErrorTreeNodeViewModel(NonLeafTreeNodeViewModel parent, Exception exception)
+        {
+            _parent = parent;
+            _exception = exception;
+        }
+
+        public override string Text => "Error: " + _exception.Message;
+
+        private DelegateCommand _retryCommand;
+        public ICommand RetryCommand => _retryCommand ??= new DelegateCommand(Retry);
+
+        private void Retry()
+        {
+            _parent.RetryLoadChildren();
+        }
     }
 }
