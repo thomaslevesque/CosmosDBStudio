4ea42610c06f32c47c59a4fb0a10d8ca29d293dc
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-12-06 00:03:09 +0100

    Move TreeNodeViewModel to separate file

--- CosmosDBStudio/ViewModel/NonLeafTreeNodeViewModel.cs
+++ CosmosDBStudio/ViewModel/NonLeafTreeNodeViewModel.cs
@@ -1,30 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using EssentialMVVM;
 
 namespace CosmosDBStudio.ViewModel
 {
-    public abstract class TreeNodeViewModel : BindableBase
-    {
-        public abstract string Text { get; }
-
-        public virtual IEnumerable<MenuCommandViewModel> MenuCommands => Enumerable.Empty<MenuCommandViewModel>();
-
-        private bool _isExpanded;
-        public bool IsExpanded
-        {
-            get => _isExpanded;
-            set => Set(ref _isExpanded, value).AndExecute(OnIsExpandedChanged);
-        }
-
-        protected virtual void OnIsExpandedChanged()
-        {
-        }
-    }
-
     public abstract class NonLeafTreeNodeViewModel : TreeNodeViewModel
     {
         protected NonLeafTreeNodeViewModel()
---
+++ CosmosDBStudio/ViewModel/TreeNodeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EssentialMVVM;
+
+namespace CosmosDBStudio.ViewModel
+{
+    public abstract class TreeNodeViewModel : BindableBase
+    {
+        public abstract string Text { get; }
+
+        public virtual IEnumerable<MenuCommandViewModel> MenuCommands => Enumerable.Empty<MenuCommandViewModel>();
+
+        private bool _isExpanded;
+        public bool IsExpanded
+        {
+            get => _isExpanded;
+            set => Set(ref _isExpanded, value).AndExecute(OnIsExpandedChanged);
+        }
+
+        protected virtual void OnIsExpandedChanged()
+        {
+        }
+    }
+}
