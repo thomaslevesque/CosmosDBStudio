70281150486ea400159ca3c0ead787dc45610d12
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2020-09-30 01:07:20 +0200

    Split ScriptEditorView into separate controls

---
+++ src/CosmosDBStudio/View/UserDefinedFunctionEditorView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CosmosDBStudio.View
+{
+    /// <summary>
+    /// Interaction logic for UserDefinedFunctionEditorView.xaml
+    /// </summary>
+    public partial class UserDefinedFunctionEditorView : UserControl
+    {
+        public UserDefinedFunctionEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
