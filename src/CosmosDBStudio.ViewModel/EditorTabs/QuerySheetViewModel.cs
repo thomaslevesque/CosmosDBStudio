3e5dac724b955b7832eb6d40f5f581dee549c636
Ricardo Pineda <ricardopinedathen@gmail.com>
2022-03-22 16:25:12 -0400

    Cleanup unused constant

--- src/CosmosDBStudio.ViewModel/EditorTabs/QuerySheetViewModel.cs
+++ src/CosmosDBStudio.ViewModel/EditorTabs/QuerySheetViewModel.cs
@@ -21,8 +21,6 @@ namespace CosmosDBStudio.ViewModel.EditorTabs
 {
     public class QuerySheetViewModel : TabViewModelBase, ISaveable
     {
-        private const string DefaultQuery = "select * from c";
-
         public static int UntitledCounter { get; set; }
         private readonly IViewModelFactory _viewModelFactory;
         private readonly IDialogService _dialogService;
